e3b403f3aa06a75d639c60e2f74dba7b0b4a1390
Phil Krull <pkrull@codingdojo.com>
2020-04-30 08:50:56 -0500

    adding day four challenges

---
+++ week_two/day_four.md
@@ -0,0 +1,16 @@
+## Week II Day VI Algorithms
+
+### Array: Remove Negatives
+    function RemoveNegs(arr){}
+        I => [4,-2,7,1,3,8,-5]
+        O => [4,7,1,3,8]
+
+### Array: SkyLine Heights
+    function SkyLineHeights(arr){}
+        I => [4,2,7,8,5]
+        O => [4,7,8]
+
+### Array: Nth-Largest
+    function NthLargest(arr, n){}
+        I => [9,4,2,1,3,5,7], 3
+        O => 5
---
+++ week_two/day_three.js
@@ -0,0 +1,26 @@
+// #### Part One
+function chainChange(num) {
+
+    // {
+            // 'q': number of quarters,
+            // 'd': number of dimes,
+            // 'n': number of nickels,
+            // 'p': number of pennies
+    // }
+    // return an object
+}
+
+// #### Part Two Array: Swap Pairs
+function SwapPairs(arr){
+
+}
+// I=>[1,2,3,4]
+// 0=>[2,1,4,3]
+
+
+// #### Part Three Array: Move Min to Front
+function MoveMinToFront(arr){
+
+}
+// I=>[4,2,1,3,5]
+// 0=>[1,4,2,3,5]
